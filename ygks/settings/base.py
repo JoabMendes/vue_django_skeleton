f1d727a90a3c4fd0e93bcf0c888d72190a08aca8
JoabMendes <joab.mendes.r2d2@gmail.com>
2018-09-13 22:58:43 -0400

    Updated whitenoise config

--- ygks/settings/base.py
+++ ygks/settings/base.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.realpath(os.path.dirname(__file__) + "/.."))
+PACKAGE_ROOT = os.path.abspath(__file__ + "/../../")
 
 
 # Quick-start development settings - unsuitable for production
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,4 +131,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- ygks/settings/production.py
+++ ygks/settings/production.py
@@ -6,8 +6,6 @@ DEBUG = False
 # Admins (For sending erros log)
 ADMINS = [('Joab Mendes', 'joab.mendes.r2d2@gmail.com')]
 
-MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
-
 if bool(os.getenv('HEROKU_ENV', False)):
     # If it's hereku just use the url
     DATABASES['default'] = dj_database_url.config()
